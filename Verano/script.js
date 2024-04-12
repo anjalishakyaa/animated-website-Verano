88cc2839fd7cb5daf8b0d863a68d348b14d4b9a2
Anjali Shakya <157213476+anjalishakyaa@users.noreply.github.com>
2024-04-12 13:58:30 +0530

    Add files via upload

---
+++ Verano/script.js
@@ -0,0 +1,86 @@
+const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+});
+
+function loadinganimation() {
+    gsap.from("#page1 h1, h3", {
+      y: 100,
+      opacity: 0,
+      delay: 0.5,
+      duration: 0.9,
+      stagger: 0.3,
+    });
+    gsap.from("#page1 #video-container", {
+      scale: 0.9,
+      opacity: 0,
+      delay: 1.3,
+      duration: 0.5,
+    });
+  }
+  loadinganimation();
+
+function cursorAnimation() {
+    document.addEventListener("mousemove", function (dets) {
+      gsap.to("#cursor", {
+        left: dets.x,
+        top: dets.y,
+      });
+    });
+    // document.querySelector("#child1").addEventListener("mouseenter",function(){
+  
+    // })
+  
+    // document.querySelector("#child1").addEventListener("mouseleave",function(){
+    //   gsap.to("#cursor",{
+    //     transform: 'translate(-50%,-50%) scale(0)'
+    //   })
+    // })
+    document.querySelectorAll(".child").forEach(function (elem) {
+      elem.addEventListener("mouseenter", function () {
+        gsap.to("#cursor", {
+          transform: "translate(-50%,-50%) scale(1)",
+        });
+      });
+      elem.addEventListener("mouseleave", function () {
+        gsap.to("#cursor", {
+          transform: "translate(-50%,-50%) scale(0)",
+        });
+      });
+    });
+  }
+  cursorAnimation();
+
+
+
+  function page4Animation() {
+    var elemC = document.querySelector("#elem-container")
+    var fixed = document.querySelector("#fixed-image")
+    elemC.addEventListener("mouseenter", function () {
+        fixed.style.display = "block"
+    })
+    elemC.addEventListener("mouseleave", function () {
+        fixed.style.display = "none"
+    })
+
+    var elems = document.querySelectorAll(".elem")
+    elems.forEach(function (e) {
+        e.addEventListener("mouseenter", function () {
+            var image = e.getAttribute("data-image")
+            fixed.style.backgroundImage = `url(${image})`
+        })
+    })
+}
+
+
+
+function swiperAnimation() {
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+  });
+}
+swiperAnimation()
+page4Animation()
+
